cc2881f6dc2c9f12c25e3a35a942b95ce66b1145
Martin Kiprotich. Ruto <MRUTO@safaricom.net>
2018-01-19 10:36:34 +0300

    delete all files in src/. create index.css and index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Square extends React.Component {
+  render() {
+    return (
+      <button classname = "square">
+        {}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square />;
+  }
+
+  render() {
+    const status = 'Next Player: X';
+
+    return (
+      <div>
+        <div classname = "status">{status}</div>
+        <div classname = "board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div classname = "board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div classname = "board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div classname = "game">
+        <div classname = "game-board">
+          <Board />
+        </div>
+        <div classname = "game-info">
+          <div>{}</div>
+          <ol>{}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render (
+  <Game />,
+  document.getElementById('root')
+);
